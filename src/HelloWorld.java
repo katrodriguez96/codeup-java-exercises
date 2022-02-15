3f541ed5f2015b183d055953995311b3f7bdd641
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-02-15 16:00:02 -0600

    java syntax exercise started

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,12 @@
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+        String myString = "hello";
+        System.out.println(myString);
+        float myNumber;
+        myNumber = 123;
+        System.out.println(myNumber);
+    }
 }
