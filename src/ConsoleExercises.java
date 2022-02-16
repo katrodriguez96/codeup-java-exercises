76008ef6b271cd19734daaa8979f068212857efa
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-02-16 12:31:13 -0600

    java console exercises done

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,34 @@
+import java.util.Scanner; // imports must be placed at the top before the class
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        System.out.printf("The value of pi is approximately %.4s.%n", pi); // don't quite understand, thought it would print 4 places after decimal point
+        // new scanner created
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n"); // idk if im using this correctly
+//        System.out.print("Enter an integer: ");
+//        int userInput = scanner.nextInt(); // will only accept an integer
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+//        System.out.print("Enter three words: "); // if user enters less than 3 words endless loop? it keeps looking for the next word?
+//        String firstWord = scanner.next();
+//        String secondWord = scanner.next();
+//        String thirdWord = scanner.next();
+//        System.out.println("You entered: --> \"" + firstWord + "\" <--\nThen: --> \"" + secondWord + "\" <--\nFinally: --> \"" + thirdWord + "\" <--");
+//        System.out.print("Enter a sentence: ");
+//        String userInput = scanner.nextLine();
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+        System.out.print("Enter the length of your room: ");
+        String userLength = scanner.nextLine();
+        System.out.print("Enter the width of your room: ");
+        String userWidth = scanner.nextLine();
+        System.out.print("Enter the height of your room: ");
+        String userHeight = scanner.nextLine();
+        double length = Double.parseDouble(userLength);
+        double width = Double.parseDouble(userWidth);
+        double height = Double.parseDouble(userHeight);
+        System.out.println("The area of your room is: " + (length * width)); // using \n to create a new line didn't work?
+        System.out.println("The perimeter of your room is: " + ((length * 2) + (width * 2)));
+        System.out.println("The volume of your room is: " + (length * width * height));
+    }
+}
