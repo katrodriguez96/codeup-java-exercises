749aefa385869f4d0aefae8686294b586c78173b
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-02-16 10:39:36 -0600

    exercise complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,5 +8,28 @@ public class HelloWorld {
         float myNumber;
         myNumber = 123;
         System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++); // prints x as 5 and THEN increments
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x); // increments and THEN prints x as 6
+//        System.out.println(x);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+//        int x = 4;
+//        x += 5; // same as x = x + 5;
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+//        y *= x; // same as y = y * x;
+//        System.out.println(y);
+        int x = 10;
+        int y = 2;
+        x /= y; // same as x = x / y
+        y -= x; // same as y = y - x
+        System.out.println(x);
+        System.out.println(y);
     }
 }
