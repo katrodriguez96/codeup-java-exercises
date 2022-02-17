71248e91c3302b169a0df8877ed4e1b0ff531213
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-02-17 13:41:36 -0600

    part two control flow exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -20,6 +20,17 @@ public class ControlFlowExercises {
 //            System.out.println("i is " + i);
 //        }
 
-
+        int i;
+        for (i = 1; i <= 100; i++)  {
+            if (i % 3 == 0 & i % 5 == 0) { // use & to check both expressions. && only checks the second if applicable
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
