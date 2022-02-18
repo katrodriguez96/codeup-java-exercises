e4c90336ee566f5f8629894a44832b44199e3509
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-02-18 10:44:35 -0600

    bob exercise done

---
+++ src/Bob.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String keepTalking;
+        do {
+            System.out.print("Say something to Bob: ");
+            String userInput = scanner.nextLine();
+            if (userInput.endsWith("?")) {
+                System.out.println("Sure.");
+            } else if (userInput.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if (userInput.equals("")) {
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever.");
+            }
+            System.out.println("Would you like to continue? [y/n]");
+            keepTalking = scanner.nextLine();
+        } while (keepTalking.equalsIgnoreCase("y"));
+
+    }
+}
