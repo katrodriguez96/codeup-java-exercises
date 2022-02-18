0d9da72e373b3f790031cd0d72c77aa58d9a3c56
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-02-18 10:24:39 -0600

    string basics java exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,18 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        String pinkFloyd = "We don't need no education\nWe don't need no thought control"; // \n = new line
+        System.out.println(pinkFloyd);
+
+        String checkOut = "Check \"this\" out!, \"s inside of \"s!"; // use \ in front of " in string to show them
+        System.out.println(checkOut);
+
+        String mainDrive = "In windows, the main drive is usually C:\\"; // \ also works for \
+        System.out.println(mainDrive);
+
+        String backSlash = "I can do backslashes \\, double backslashes \\\\,\n" + // one \ for each character
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(backSlash);
+
+    }
+}
