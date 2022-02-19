d91a0418be9d96bc975d70bf671e63a2916c7966
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-02-19 17:13:49 -0600

    revisited first couple java exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,16 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // intro to java exercise
+        System.out.print("Hello, "); // will print
+        System.out.print("World!"); // on same line
+        System.out.println("\nHello, World!"); // same output as above
+
+        // syntax, types, and variables exercise
         int myFavoriteNumber = 7;
         System.out.println(myFavoriteNumber);
         String myString = "hello";
         System.out.println(myString);
-        float myNumber;
-        myNumber = 123;
+        double myNumber = 3.14;
         System.out.println(myNumber);
 //        int x = 5;
 //        System.out.println(x++); // prints x as 5 and THEN increments
@@ -14,10 +18,11 @@ public class HelloWorld {
 //        int x = 5;
 //        System.out.println(++x); // increments and THEN prints x as 6
 //        System.out.println(x);
+//        String class; // variable cannot be named 'class'
 //        String theNumberThree = "three";
-//        Object o = theNumberThree;
-//        int three = (int) o;
-//        int three = (int) "three";
+//        Object o = theNumberThree; // as an Object, it is still a String
+//        int three = (int) o; // and cannot be cast as an Integer
+//        int three = (int) "three"; // same as above without casting to an Object
 //        int x = 4;
 //        x += 5; // same as x = x + 5;
 //        System.out.println(x);
@@ -31,5 +36,9 @@ public class HelloWorld {
         y -= x; // same as y = y - x
         System.out.println(x);
         System.out.println(y);
+        System.out.println(Integer.MAX_VALUE); // returns 2147483647
+        System.out.println(Integer.MIN_VALUE); // returns -2147483648
+        System.out.println(Integer.MAX_VALUE + 1); // returns -2147483648
+        System.out.println(Integer.MIN_VALUE - 1); // returns 2147483647
     }
 }
