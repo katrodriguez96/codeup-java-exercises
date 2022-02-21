1fef1aebfa9900ce96f7331f9cbc331c82d61b6e
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-02-21 15:04:50 -0600

    added counter to high or low exercise

--- src/HighLow.java
+++ src/HighLow.java
@@ -2,22 +2,27 @@ import java.util.Scanner;
 
 public class HighLow {
     public static void main(String[] args) {
-        int randomNum;
         int max = 100;
         int min = 1;
-        randomNum = (int) (Math.random() * (max - min + min) + 1);
+        int randomNum = (int) (Math.random() * (max - min + min) + 1);
 //        System.out.printf("Random number from 1 to 100: %d", randomNum);
         int userGuess;
+        int count = 0;
         do {
             Scanner scanner = new Scanner(System.in);
             System.out.print("Guess a number between 1 and 100: ");
             userGuess = scanner.nextInt();
+            count++;
             if (userGuess > randomNum) {
                 System.out.println("HIGHER");
             } else if (userGuess < randomNum) {
                 System.out.println("LOWER");
             } else {
-                System.out.println("GOOD GUESS!");
+                if (count == 1) {
+                    System.out.println("WOW FIRST TRY!");
+                } else {
+                    System.out.printf("GOOD GUESS! And it only took %d tries :)", count);
+                }
             }
         } while (userGuess != randomNum);
     }
