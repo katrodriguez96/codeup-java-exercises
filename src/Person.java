0b8f5561b5f56780d21f530a863f9ed6e9a117a7
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-02-22 15:06:56 -0600

    finished object basics exercise

--- src/Person.java
+++ src/Person.java
@@ -19,8 +19,25 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        Person testPerson = new Person("Kat");
-        System.out.println("testPerson.getName() = " + testPerson.getName());
-        testPerson.sayHello();
+//        Person testPerson = new Person("Kat");
+//        System.out.println("testPerson.getName() = " + testPerson.getName());
+//        testPerson.sayHello();
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName())); // true. the name value is the same string
+//        System.out.println(person1 == person2); // false. they are declared separately
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1; // we are declaring person2 IS person1. same name value, same object value
+//        System.out.println(person1 == person2); // always returns true
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName()); // "John"
+        System.out.println(person2.getName()); // "John"
+        person2.setName("Jane");
+        System.out.println(person1.getName()); // "Jane" since person 2 IS person 1, changing the name value affects the same object
+        System.out.println(person2.getName()); // "Jane"
     }
 }
