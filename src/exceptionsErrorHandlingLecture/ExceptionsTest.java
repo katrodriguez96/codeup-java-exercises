cf1810f4073ae0c15042a92d96cf5ef92e842f38
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-03-08 10:03:07 -0600

    started lecture notes

---
+++ src/exceptionsErrorHandlingLecture/ExceptionsTest.java
@@ -0,0 +1,24 @@
+package exceptionsErrorHandlingLecture;
+
+public class ExceptionsTest {
+    public static void main(String[] args) throws Exception {
+
+//        throw new Exception("Something went wrong!");
+//        // anything below exception becomes unreachable
+//        System.out.println("Good morning Sirius!");
+
+        // try-catch block
+        try {
+            // Code to break application here
+            throw new NullPointerException("Something broke while running the application!");
+        } catch (NullPointerException exceptionObject) {
+            System.out.println("This is a null pointer exception.");
+            System.out.println("Here's an example of .getMessage() " + exceptionObject.getMessage());
+            exceptionObject.printStackTrace();
+        } catch (RuntimeException exceptionObject) {
+            System.out.println("This is a runtime exception.");
+            System.out.println("Here's an example of .getMessage() " + exceptionObject.getMessage());
+            exceptionObject.printStackTrace();
+        }
+    }
+}
